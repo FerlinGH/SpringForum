807510f06edd67fb294d5159f2bdcce42acdb9ee
Dmytro Grygorenko <grygorenko.d@ukr.net>
2017-11-04 21:24:30 +0200

    update

--- src/main/java/net/ukr/grygorenko_d/springforum/config/WebMvcConfig.java
+++ src/main/java/net/ukr/grygorenko_d/springforum/config/WebMvcConfig.java
@@ -1,8 +1,19 @@
 package net.ukr.grygorenko_d.springforum.config;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -19,5 +30,46 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		resolver.setSuffix(".jsp");
 		resolver.setOrder(1);
 		return resolver;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/springforum?useSSL=false");
+		dataSource.setUsername("springstudent");
+		dataSource.setPassword("springstudent");
+
+		return dataSource;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+
+		return adapter;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,
+			JpaVendorAdapter adapter) {
+		Properties jpaProp = new Properties();
+		jpaProp.put("hibernate.hbm2ddl.auto", "create-drop");
+
+		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactory.setDataSource(dataSource);
+		entityManagerFactory.setJpaVendorAdapter(adapter);
+		entityManagerFactory.setJpaProperties(jpaProp);
+		entityManagerFactory.setPackagesToScan("net.ukr.grygorenko_d.springforum");
+
+		return entityManagerFactory;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
 	}
 }
